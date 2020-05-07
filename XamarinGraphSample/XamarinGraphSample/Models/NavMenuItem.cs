50be7948f6a6bde6f10219a6c687430be40d2b81
Dara Oladapo <daraoladapo@outlook.com>
2020-05-07 02:54:59 +0100

    Pages in place

---
+++ XamarinGraphSample/XamarinGraphSample/Models/NavMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinGraphSample.Models
+{
+    public enum MenuItemType
+    {
+        Welcome,
+        Calendar
+    }
+
+    public class NavMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
