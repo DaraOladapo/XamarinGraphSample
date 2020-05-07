f842d5e69453a9975620f42fa84e4684b50d6044
Dara Oladapo <daraoladapo@outlook.com>
2020-05-07 02:39:24 +0100

    place hodlers adn functions

--- XamarinGraphSample/XamarinGraphSample/App.xaml.cs
+++ XamarinGraphSample/XamarinGraphSample/App.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace XamarinGraphSample
 {
-    public partial class App : Application
+    public partial class App : Application, INotifyPropertyChanged
     {
         public App()
         {
@@ -23,6 +27,85 @@ namespace XamarinGraphSample
 
         protected override void OnResume()
         {
+        }
+        // Is a user signed in?
+        private bool isSignedIn;
+        public bool IsSignedIn
+        {
+            get { return isSignedIn; }
+            set
+            {
+                isSignedIn = value;
+                OnPropertyChanged("IsSignedIn");
+                OnPropertyChanged("IsSignedOut");
+            }
+        }
+
+        public bool IsSignedOut { get { return !isSignedIn; } }
+
+        // The user's display name
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        // The user's email address
+        private string userEmail;
+        public string UserEmail
+        {
+            get { return userEmail; }
+            set
+            {
+                userEmail = value;
+                OnPropertyChanged("UserEmail");
+            }
+        }
+
+        // The user's profile photo
+        private ImageSource userPhoto;
+        public ImageSource UserPhoto
+        {
+            get { return userPhoto; }
+            set
+            {
+                userPhoto = value;
+                OnPropertyChanged("UserPhoto");
+            }
+        }
+        public async Task SignIn()
+        {
+            await GetUserInfo();
+
+            IsSignedIn = true;
+        }
+
+        public async Task SignOut()
+        {
+            UserPhoto = null;
+            UserName = string.Empty;
+            UserEmail = string.Empty;
+            IsSignedIn = false;
+        }
+
+        private async Task GetUserInfo()
+        {
+            //TODO: Get real image from stream
+            //UserPhoto = ImageSource.FromStream(() => GetUserPhoto());
+            UserPhoto = ImageSource.FromUri(new Uri("https://www.dropbox.com/s/g94h6ij1p78nij2/Dara-Avatar.jpg"));
+            UserName = "Adele Vance";
+            UserEmail = "adelev@contoso.com";
+        }
+
+        private Stream GetUserPhoto()
+        {
+            // Return the default photo
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream("GraphTutorial.no-profile-pic.png");
         }
     }
 }
